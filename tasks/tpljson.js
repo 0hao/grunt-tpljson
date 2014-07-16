09d4425a9c16ffd4178bd7341666b049e9e02dcf
林豪(046111) <hao.lin@alibaba-inc.com>
2014-07-16 14:39:44 +0800

    update0.2.0

--- tasks/tpljson.js
+++ tasks/tpljson.js
@@ -9,28 +9,33 @@
 'use strict';
 var chalk = require('chalk');
 var prettyBytes = require('pretty-bytes');
-var minify = require('html-minifier').minify;
+//var minify = require('html-minifier').minify;
 
 module.exports = function (grunt) {
+  // Internal lib.
+  var comment = require('./lib/comment').init(grunt);
+  
   grunt.registerMultiTask('tpljson', '.tpl 2 .js(json)', function () {
     var options = this.options();
 
     var files_map  = {};
+
+    var name_reg = new RegExp('/(\\w+)\\' + options.srcExt);
+
+    var srcExt_reg = new RegExp(options.srcExt+'$');
+
     grunt.file.recurse(options.src, function callback(abspath, rootdir, subdir, filename) {
-      if(subdir && /.tpl$/.test(filename)) {
+
+      if(subdir && srcExt_reg.test(filename)) {
         files_map[subdir] = files_map[subdir] || [];
-        var arr = new Array(abspath)
         files_map[subdir].push(abspath);
       }
       
-    })
-
+    });
+    
     //loop dir
     for (var key in files_map) {
-      minFiles(key);
-    };
-
-    function minFiles(key){
+      //minFiles(key);
 
       var files = files_map[key];
 
@@ -54,24 +59,41 @@ module.exports = function (grunt) {
           src = comment.stripBanner(src, options.stripBanners);
         }
 
+        var name_r = filepath.match(name_reg);
+
+
+        if(name_r) {
+          name_r = name_r[1];
+        }
+
+        //去除换行和空格
+        src = src.split('\n');
+        var t_src = [];
+        src.forEach(function(item){
+          t_src.push(item.trim());
+        });
+
+        src = "'" + name_r + "':" + "'" + t_src.join('') + "'";
+
         return src;
       }).join(options.separator);
 
-      var multi_min;
-      try {
-        multi_min = minify(multi_src, options);
-      } catch (err) {
-        return grunt.warn(key + '\n' + err);
-      }
+      // var multi_min;
+      // try {
+      //   multi_min = minify(multi_src, options);
+      // } catch (err) {
+      //   return grunt.warn(key + '\n' + err);
+      // }
 
-      if (multi_min.length < 1) {
-        grunt.log.warn('Destination not written because multi_min was empty.');
+      if (multi_src.length < 1) {
+        grunt.log.warn('Destination not written because multi_src was empty.');
       } else {
-        var destfile = options.dest + '/'+key + options.ext;
-        grunt.file.write( destfile, options.jsonp + '({' + multi_min + '})' );
-        grunt.log.writeln('done ' + chalk.cyan(destfile) + ' ' + prettyBytes(multi_min.length));
+        var destfile = options.dest + '/'+key + options.ext,  
+            srcfile = options.jsonp + '({' + multi_src + '})';
+        grunt.file.write( destfile, srcfile);
+        grunt.log.writeln('done ' + chalk.cyan(destfile) + ' ' + prettyBytes(srcfile.length));
       }
-    }
+    };
 
   });
 };
